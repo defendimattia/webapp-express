554d1da45c9ae3acc896b7fcd362e1f4c70d2d1a
defendimattia <defendi.mattia92@gmail.com>
2025-05-09 15:41:34 +0200

    add: connection to database and env login

--- routers/movies.js
+++ routers/movies.js
@@ -1,12 +1,27 @@
 const express = require("express")
 const router = express.Router()
+const connection = require("../data/book-db")
 
 router.get("/", (req, res) => {
-    res.send("movies list")
+
+    sql = "SELECT * FROM movies"
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: "Database query failed" })
+        res.json(results)
+    })
 })
 
 router.get("/:id", (req, res) => {
-    res.send("single movie details")
+
+    const id = req.params.id
+
+    sql = "SELECT * FROM movies WHERE id = ?"
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: "Database query failed" })
+        if (results.length === 0) return res.status(404).json({ error: "Movie not found" })
+        res.json(results[0])
+    })
 })
+
 
 module.exports = router
