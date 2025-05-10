3a9addad44ec27ae19a2cbc91fec6619b185129e
defendimattia <defendi.mattia92@gmail.com>
2025-05-10 11:19:35 +0200

    add: server port on .env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.SERVER_PORT
 const moviesRouter = require("./routers/moviesRoutes")
 
 app.get("/", (req, res) => {
