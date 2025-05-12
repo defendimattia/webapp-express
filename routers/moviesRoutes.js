b5c970122f41ef9b27a4ac6222fc1edab7a09f2c
defendimattia <defendi.mattia92@gmail.com>
2025-05-12 09:56:46 +0200

    add: comments on moviesRouters,js

--- routers/moviesRoutes.js
+++ routers/moviesRoutes.js
@@ -2,8 +2,11 @@ const express = require("express")
 const router = express.Router()
 const moviesController = require("../controllers/moviesController")
 
+
+// index
 router.get("/", moviesController.index)
 
+// show
 router.get("/:id", moviesController.show)
 
 
