8d336f59013b6442eb25252a069606146e410de0
defendimattia <defendi.mattia92@gmail.com>
2025-05-12 09:46:44 +0200

    add: comments on app.js and fix middlewares order

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require("express")
 const app = express()
 const port = process.env.SERVER_PORT
+
+// routers
 const moviesRouter = require("./routers/moviesRoutes")
+
+// middlewares
 const errorsHandler = require("./middlewares/errorsHandler")
 const notFound = require("./middlewares/notFound")
 
+// middleware static asstets
 app.use(express.static('public'))
 
+
+// welcome page
 app.get("/", (req, res) => {
     res.send("init")
 })
 
+
+// movies
 app.use("/api/movies", moviesRouter)
 
-app.use(notFound)
+
+// use 500
 app.use(errorsHandler)
+// use 404
+app.use(notFound)
+
 
 app.listen(port, () => {
     console.log("CHECK! express on port " + port)
