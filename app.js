3333b8e2bb42eac178f8b05c01c9a4efb5dc2524
defendimattia <defendi.mattia92@gmail.com>
2025-05-12 14:48:20 +0200

    fix: misspell

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const port = process.env.SERVER_PORT
-const cosr = require("cors")
+const cors = require("cors")
 
 // routers
 const moviesRouter = require("./routers/moviesRoutes")
